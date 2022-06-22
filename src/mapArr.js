6c554607236baa631aa70567ff796d957e2885b7
AbzMijon <egorchiktop228@gmail.com>
2022-06-22 17:28:10 +0300

    fixed mistakes

--- src/mapArr.js
+++
@@ -1,9 +0,0 @@
-export const SomeFunc = function(name) {
-    if(isNaN(name)) {
-        return `Hello, ${name}!`;
-    }   else if(!isNaN(name)) {
-        throw new Error('name is NAN');
-    }   else {
-        throw new Error('404 ;(');
-    }
-}

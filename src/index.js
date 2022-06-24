c9fe939bed26aa489e3be0f962e2632f97e64bb3
AbzMijon <egorchiktop228@gmail.com>
2022-06-24 14:45:15 +0300

    test git rep

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App.jsx'; 
 
+console.log('test clone git rep');
+
 ReactDom.render(<App/>, document.getElementById('root'));

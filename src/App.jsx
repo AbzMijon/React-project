b764be8be6dbf7b51d9a59daa44786a522c47329
AbzMijon <egorchiktop228@gmail.com>
2022-07-03 22:55:01 +0300

    delete GlobalThemeProvider.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import React from 'react';
 //Components
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
-import GlobalThemeProvider from './HOC/GlobalThemeProvider'
 
 function App() {
 		return (
             <React.Fragment>
-                {/* <GlobalThemeProvider> */}
                     <Header />
                     <Main />
-                {/* </GlobalThemeProvider> */}
             </React.Fragment>
         )
 }
--- src/HOC/GlobalThemeProvider.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function GlobalThemeProvider(props) {
-}
-
-export default GlobalThemeProvider;

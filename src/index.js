6ce47f4e27c6e0d048f71a80e9fca081cd5f33f5
AbzMijon <egorchiktop228@gmail.com>
2022-08-29 18:30:26 +0300

    rename commnStyles into common

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App'; 
-import './CommonStyles/style.scss';
+import './common/style.scss';
 
 const root = ReactDom.createRoot(
     document.getElementById("root")

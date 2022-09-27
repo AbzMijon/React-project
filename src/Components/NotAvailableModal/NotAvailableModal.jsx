f8dbdd0710e3b9c36245f80a136cc5e56423d970
AbzMijon <egorchiktop228@gmail.com>
2022-09-27 14:49:27 +0300

    lead down registry scss files

--- src/Components/NotAvailableModal/NotAvailableModal.jsx
+++ src/Components/NotAvailableModal/NotAvailableModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
-import './notAvailableModa.scss';
+import './notAvailableModal.scss';
 
 function NotAvailableModal({ bookSrc, valueOfAvailableModal, setValueOfAvailableModal }) {
 
